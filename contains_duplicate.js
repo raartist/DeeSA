572c003206fa10469d52e7bd264c3661bcee2715
raartist <rishabhzee98@gmail.com>
2024-09-23 10:45:42 +0530

    bruteforce approach

--- contains_duplicate.js
+++ contains_duplicate.js
@@ -1,1 +1,17 @@
 // we'll learn if our array contains duplicate elements
+
+// approach1- two nested for loops and if finds return true;
+export default function ifDuplicate(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+const arr1 = [1, 2, 3, 1];
+const result = ifDuplicate(arr1);
+console.log("Contains Duplicate element? ", result);
